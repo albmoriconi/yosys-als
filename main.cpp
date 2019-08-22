60a70f3ac283eb18bab38e481d82bd61cfd1ecd0
Alberto Moriconi <albmoriconi@gmail.com>
2019-08-22 12:59:56 +0200

    Z3 exact synthesis

--- main.cpp
+++ main.cpp
@@ -20,10 +20,6 @@
 #include "kernel/yosys.h"
 #include "kernel/sigtools.h"
 #include "z3++.h"
-#include <cmath>
-#include <cstdarg>
-#include <string>
-#include <vector>
 
 USING_YOSYS_NAMESPACE
 PRIVATE_NAMESPACE_BEGIN
@@ -31,7 +27,7 @@ PRIVATE_NAMESPACE_BEGIN
 // TODO Find a place for this
 // TODO Document me
 // https://stackoverflow.com/a/49812018
-std::string vformat(const char * const zcFormat, ...) {
+std::string vformat(const char* const zcFormat, ...) {
     va_list vaArgs;
     va_start(vaArgs, zcFormat);
     va_list vaArgsCopy;
@@ -45,15 +41,37 @@ std::string vformat(const char * const zcFormat, ...) {
 }
 
 // TODO Document me
-inline bool truth_value(int i, int t) {
+inline bool truth_value(const int i, const int t) {
     if (i == 0)
         return false;
+
     return t % static_cast<int>(pow(2, i)) >= static_cast<int>(pow(2, i - 1));
 }
 
 // TODO Document me
-void smt_lut_synthesis(const std::string& fun_spec) {
-    size_t num_vars = static_cast<int>(ceil(log2(fun_spec.size())));
+inline bool truth_value(const char c) {
+    return c != '0';
+}
+
+// TODO Document me
+void function_semantics_constraints(z3::solver &slv, const string &fun_spec, const int ax_degree,
+                                    const std::vector<z3::expr_vector> &b, const z3::expr &out_p) {
+    if (ax_degree == 0)
+        for (size_t t = 0; t < fun_spec.size(); t++)
+            slv.add(b.back()[t] == (!out_p != slv.ctx().bool_val(truth_value(fun_spec[t]))));
+    else {
+        z3::expr_vector ax(slv.ctx());
+        for (size_t t = 0; t < fun_spec.size(); t++) {
+            ax.push_back(slv.ctx().bool_const(vformat("ax_%d", t).c_str()));
+            slv.add(ax[t] == (b.back()[t] != (!out_p != slv.ctx().bool_val(truth_value(fun_spec[t])))));
+            slv.add(z3::atmost(ax, ax_degree));
+        }
+    }
+}
+
+// TODO Document me
+void smt_lut_synthesis(const std::string &fun_spec, const int ax_degree) {
+    auto num_vars = static_cast<size_t>(ceil(log2(fun_spec.size())));
     z3::context ctx;
     z3::solver slv(ctx);
 
@@ -66,6 +84,63 @@ void smt_lut_synthesis(const std::string& fun_spec) {
             slv.add(b[i][t] == ctx.bool_val(truth_value(i, t)));
         }
     }
+
+    // Input values, AIG structure and polarity
+    std::vector<std::vector<z3::expr_vector>> a(2);
+    std::vector<z3::expr_vector> s;
+    s.emplace_back(z3::expr_vector(ctx));
+    s.emplace_back(z3::expr_vector(ctx));
+    std::vector<z3::expr_vector> p;
+    p.emplace_back(z3::expr_vector(ctx));
+    p.emplace_back(z3::expr_vector(ctx));
+
+    // Function semantics
+    z3::expr out_p = ctx.bool_const("p");
+    slv.push();
+    function_semantics_constraints(slv, fun_spec, ax_degree, b, out_p);
+
+    while (!slv.check()) {
+        // Update index
+        size_t i = b.size();
+        size_t i_gates = i - (num_vars + 1);
+
+        // Drop old function semantics constraints
+        slv.pop();
+
+        // Add lists for t entries for gate i
+        b.emplace_back(z3::expr_vector(ctx));
+        for (auto &vec : a)
+            vec.emplace_back(z3::expr_vector(ctx));
+
+        // Structure (no cycles, order, polarity)
+        for (size_t c = 0; c < s.size(); c++) {
+            s[c].push_back(ctx.int_const(vformat("s_%d_%d", c, i).c_str()));
+            slv.add(s[c][i_gates] < static_cast<int>(i));
+            slv.add(s[c][i_gates] >= 0);
+            p[c].push_back(ctx.bool_const(vformat("p_%d_%d", c, i).c_str()));
+        }
+        slv.add(s[0][i_gates] < s[1][i_gates]);
+
+        for (size_t t = 0; t < fun_spec.size(); t++) {
+            // AND functionality
+            b[i].push_back(ctx.bool_const(vformat("b_%d_%d", i, t).c_str()));
+            for (size_t c = 0; c < s.size(); c++)
+                a[c][i_gates].push_back(ctx.bool_const(vformat("a_%d_%d_%d", c, i, t).c_str()));
+            slv.add(b[i][t] == (a[0][i_gates][t] && a[1][i_gates][t]));
+
+            // Input connections
+            for (size_t j = 0; j < i; j++)
+                for (size_t c = 0; c < s.size(); c++)
+                    slv.add(z3::implies(s[c][i_gates] == static_cast<int>(j),
+                                        a[c][i_gates][t] == (b[j][t] != !p[c][i_gates])));
+        }
+
+        // Update function semantics
+        slv.push();
+        function_semantics_constraints(slv, fun_spec, ax_degree, b, out_p);
+    }
+
+    std::cout << "\n" << slv.get_model() << "\n";
 }
 
 struct Als : public Pass {
@@ -122,7 +197,7 @@ struct Als : public Pass {
                             cell->getParam("\\LUT").as_string().c_str(), cell->name.c_str());
 
         // 5. SMT LUT synthesis
-        smt_lut_synthesis("0111100010001000");
+        smt_lut_synthesis("0111100010001000", 0);
 
         log_pop();
     }
