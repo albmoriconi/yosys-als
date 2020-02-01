61e2d508879b2d76dcb31662e2ef1450355ddce6
Alberto Moriconi <albmoriconi@gmail.com>
2020-02-01 19:30:08 +0100

    dbcreate configurable

--- dbcreate/main.cpp
+++ dbcreate/main.cpp
@@ -1,18 +1,36 @@
-//
-// Created by alberto on 25/01/20.
-//
+/* -*- c++ -*-
+ *  dbcreate -- Exact LUT synthesis database creator
+ *
+ *  Copyright (C) 2020  Alberto Moriconi <albmoriconi@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
 
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <bitset>
-#include <thread>
-#include <boost/dynamic_bitset.hpp>
+/**
+ * @file
+ * @brief Entry point for dbcreate
+ */
+
 #include "smtsynth.h"
 
-constexpr size_t db2_size = 16;
-constexpr size_t db3_size = 512;
-constexpr size_t db4_size = 65536;
+#include <boost/dynamic_bitset.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <string>
+#include <thread>
 
 void dump_aig(const yosys_als::mig_model_t &model, std::ostream &out_file) {
     std::string fun_spec;
@@ -28,96 +46,87 @@ void dump_aig(const yosys_als::mig_model_t &model, std::ostream &out_file) {
     out_file << model.out_p << "\n";
 }
 
-int main() {
-    yosys_als::mig_model_t database2[db2_size];
-    yosys_als::mig_model_t database3[db3_size];
-    yosys_als::mig_model_t database4[db4_size];
-
-//    for (size_t i = 0; i < db2_size; i++) {
-//        std::string spec_str = std::bitset<4>(i).to_string();
-//        //std::cout << "Synthesizing " << spec_str << "... ";
-//        database2[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-//        //std::cout << "done with " << database2[i].num_gates << " gates\n";
-//    }
-//
-//    std::cout << "LUT2 database done\n";
-//    std::ofstream my_file2("db2.bin");
-//    if (!my_file2.is_open()) {
-//        std::cout << "error" << std::endl;
-//        return 1;
-//    }
-//    for (const auto &i : database2)
-//        dump_aig(i, my_file2);
-//    my_file2.close();
-//    std::cout << "LUT2 database dumped\n";
-//
-//    for (size_t i = 0; i < db3_size; i++) {
-//        std::string spec_str = std::bitset<8>(i).to_string();
-//        //std::cout << "Synthesizing " << spec_str << "... ";
-//        database3[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-//        //std::cout << "done with " << database3[i].num_gates << " gates\n";
-//    }
-//
-//    std::cout << "LUT3 database done\n";
-//    std::ofstream my_file3("db3.bin");
-//    if (!my_file3.is_open()) {
-//        std::cout << "error" << std::endl;
-//        return 1;
-//    }
-//    for (const auto &i : database3)
-//        dump_aig(i, my_file3);
-//    my_file3.close();
-//    std::cout << "LUT3 database dumped\n";
-
-    std::thread t1([&]() {
-        for (size_t i = 0; i < db4_size; i += 8) {
-            std::string spec_str = std::bitset<16>(i).to_string();
-            std::cout << "Synthesizing " << spec_str << "... ";
-            database4[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-            std::cout << "done with " << database4[i].num_gates << " gates\n";
-        }
-    });
-
-    std::thread t2([&]() {
-        for (size_t i = 2; i < db4_size; i += 8) {
-            std::string spec_str = std::bitset<16>(i).to_string();
-            std::cout << "Synthesizing " << spec_str << "... ";
-            database4[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-            std::cout << "done with " << database4[i].num_gates << " gates\n";
-        }
-    });
-
-    std::thread t3([&]() {
-        for (size_t i = 4; i < db4_size; i += 8) {
-            std::string spec_str = std::bitset<16>(i).to_string();
-            std::cout << "Synthesizing " << spec_str << "... ";
-            database4[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-            std::cout << "done with " << database4[i].num_gates << " gates\n";
-        }
-    });
-
-    std::thread t4([&]() {
-        for (size_t i = 6; i < db4_size; i += 8) {
-            std::string spec_str = std::bitset<16>(i).to_string();
-            std::cout << "Synthesizing " << spec_str << "... ";
-            database4[i] = yosys_als::synthesize_lut(boost::dynamic_bitset<>(spec_str), 0);
-            std::cout << "done with " << database4[i].num_gates << " gates\n";
-        }
-    });
-
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-
-    std::cout << "LUT4 database done\n";
-    std::ofstream my_file4("db4.bin");
-    if (!my_file4.is_open()) {
-        std::cout << "error" << std::endl;
+int main(int argc, char *argv[]) {
+    // TODO Use Boost.Program_options for arguments
+    if (argc != 5) {
+        std::cout << "Expected 5 arguments" << std::endl;
         return 1;
     }
-    for (const auto &i : database4)
-        dump_aig(i, my_file4);
-    my_file4.close();
-    std::cout << "LUT4 database dumped\n";
+
+    size_t db_lutn = strtol(argv[1], nullptr, 10);
+    size_t spec_bits = 1u << db_lutn;
+    size_t max_db_size = 1u << (db_lutn * db_lutn);
+    size_t db_start = strtol(argv[2], nullptr, 10);
+    size_t db_end = strtol(argv[3], nullptr, 10);
+    size_t n_threads = strtol(argv[4], nullptr, 10);
+
+    if (db_end == 0 || db_end > max_db_size)
+        db_end = max_db_size;
+
+    size_t db_size = db_end - db_start;
+
+    if (db_lutn < 2) {
+        std::cout << "LUT parameter should be at least 2" << std::endl;
+        return -1;
+    }
+    if (n_threads < 1) {
+        std::cout << "Thread parameter should be at least 1" << std::endl;
+        return -2;
+    }
+    if (db_start >= db_end) {
+        std::cout << "No database entries selected" << std::endl;
+        return -3;
+    }
+
+    std::cout << "Generating database for " << db_lutn << "-LUTs\n";
+    std::cout << "Entries: [" << db_start << "..." << db_end - 1 << "]\n";
+    std::cout << "Size: " << db_size << "\n";
+    std::cout << "Threads: " << n_threads << "\n";
+
+    yosys_als::mig_model_t database[db_size];
+
+    std::vector<std::thread> threads;
+    std::mutex print_mutex;
+    std::condition_variable all_spawned;
+
+    for (size_t t = 0; t < n_threads; t++) {
+        size_t db_slice = db_size / n_threads;
+        size_t t_start = db_start + t * db_slice;
+        size_t t_end = t == n_threads - 1 ? db_end : t_start + db_slice;
+
+        print_mutex.lock();
+        std::cout << "\nSpawning thread " << t << "\n";
+        std::cout << "Entries: [" << t_start << "..." << t_end - 1 << "]" << "\n";
+
+        threads.emplace_back([t, t_start, t_end, spec_bits, &print_mutex, &database]() {
+            print_mutex.lock();
+            print_mutex.unlock();
+            for (size_t i = t_start; i < t_end; i++) {
+                auto spec = boost::dynamic_bitset<>(spec_bits, i);
+                database[i] = yosys_als::synthesize_lut(spec, 0);
+
+                std::string spec_s;
+                boost::to_string(spec, spec_s);
+                print_mutex.lock();
+                std::cout << "\nThread " << t << ": ";
+                std::cout << spec_s << " done with " << database[i].num_gates << " gates";
+                print_mutex.unlock();
+            }
+        });
+        print_mutex.unlock();
+    }
+
+    for (auto &t : threads)
+        t.join();
+
+    std::cout << "\n\nDatabase done\n";
+    std::ofstream my_file("db.bin");
+    if (!my_file.is_open()) {
+        std::cout << "IO error" << std::endl;
+        return 1;
+    }
+    for (const auto &i : database)
+        dump_aig(i, my_file);
+    my_file.close();
+    std::cout << "Database dumped\n";
 }
