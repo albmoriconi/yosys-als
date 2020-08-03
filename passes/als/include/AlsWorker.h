4e620a96eccafd1866b5803d16b8d61da3245e56
Alberto Moriconi <albmoriconi@gmail.com>
2020-08-03 20:55:53 +0200

    Changed project structure

---
+++ passes/als/include/AlsWorker.h
@@ -0,0 +1,99 @@
+/* -*- c++ -*-
+ *  yosys-als -- Approximate logic synthesis
+ *
+ *  Copyright (C) 2019  Alberto Moriconi <albmoriconi@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+/**
+ * @file
+ * @brief pproximate logic synthesis worker for Yosys ALS module
+ */
+
+#ifndef YOSYS_ALS_ALSWORKER_H
+#define YOSYS_ALS_ALSWORKER_H
+
+#include "Optimizer.h"
+#include "smtsynth.h"
+
+#include "kernel/yosys.h"
+
+#include <sqlite3.h>
+
+#include <string>
+#include <vector>
+
+namespace yosys_als {
+
+    /**
+     * @brief Worker for the ALS pass
+     */
+    class AlsWorker {
+    public:
+        /// If \c true, log debug information
+        bool debug = false;
+
+        /// If \c true, rewrite AIG
+        bool rewrite_run = false;
+
+        /// Weights for the outputs
+        weights_t weights;
+
+        /// Maximum number of iterations for the optimizer
+        size_t max_iter {};
+
+        /// Number of test vectors to be evaluated
+        size_t test_vectors_n {};
+
+        /// Index of the synthesized LUTs
+        Yosys::dict<Yosys::Const, std::vector<aig_model_t>> synthesized_luts;
+
+        /**
+         * Runs an ALS step on selected module
+         * @param module A module
+         */
+        void run(Yosys::Module *const module);
+
+    private:
+        sqlite3 *db = nullptr;
+
+        template <typename E>
+        static std::string print_archive(const Optimizer<E> &opt, const archive_t<E> &arch) {
+            std::string log_string;
+            log_string.append(" Entry     Chosen LUTs         Arel        Gates\n");
+            log_string.append(" ----- --------------- ------------ ------------\n");
+
+            char log_line_buffer[60];
+            for (size_t i = 0; i < arch.size(); i++) {
+                auto choice_s = opt.to_string(arch[i].first);
+                if (choice_s.size() > 15) {
+                    choice_s = choice_s.substr(0, 15);
+                }
+                snprintf(log_line_buffer, sizeof(log_line_buffer), " %5zu %15s %12g %12g\n",
+                         i, choice_s.c_str(), arch[i].second[0], arch[i].second[1]);
+                log_string.append(log_line_buffer);
+            }
+
+            return log_string;
+        }
+
+        void replace_lut(Yosys::Module *const module, Yosys::Cell *const lut, const aig_model_t &aig);
+
+        void exact_synthesis_helper(Yosys::Module *module);
+    };
+
+}
+
+#endif //YOSYS_ALS_ALSWORKER_H
---
+++ passes/als/source/als.cc
@@ -0,0 +1,143 @@
+/* -*- c++ -*-
+ *  yosys-als -- Approximate logic synthesis
+ *
+ *  Copyright (C) 2019  Alberto Moriconi <albmoriconi@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+/**
+ * @file
+ * @brief Approximate logic synthesis pass for Yosys ALS module
+ */
+
+#include "kernel/yosys.h"
+
+#include "AlsWorker.h"
+
+USING_YOSYS_NAMESPACE
+
+/**
+ * @brief Yosys ALS module namespace
+ */
+namespace yosys_als {
+
+    /**
+     * \brief Yosys ALS pass
+     */
+    struct AlsPass : public Pass {
+        AlsPass() : Pass("als", "approximate logic synthesis") {}
+
+        void help() YS_OVERRIDE {
+            //   |---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|
+            log("\n");
+            log("    als [options] [selection]\n");
+            log("\n");
+            log("This command executes an approximate logic synthesis.\n");
+            log("\n");
+            log("    -w <signal> <value>\n");
+            log("        set the weight for the output signal to the specified power of two.\n");
+            log("\n");
+            log("\n");
+            log("    -i <value>\n");
+            log("        set the number of iterations for the optimizer.\n");
+            log("\n");
+            log("\n");
+            log("    -v <value>\n");
+            log("        set the number of test vectors for the evaluator.\n");
+            log("\n");
+            log("\n");
+            log("    -r\n");
+            log("        run AIG rewriting of top module\n");
+            log("\n");
+            log("\n");
+            log("    -d\n");
+            log("        enable debug output\n");
+            log("\n");
+        }
+
+        void execute(std::vector<std::string> args, Design *design) YS_OVERRIDE {
+            log_header(design, "Executing ALS pass (approximate logic synthesis).\n");
+            log_push();
+
+            AlsWorker worker;
+            std::vector<std::pair<std::string, std::string>> weights;
+            std::string max_iter = "2500";
+            std::string test_vectors_n = "1000";
+
+            // TODO Add arguments for specifying input probability
+            size_t argidx;
+            for (argidx = 1; argidx < args.size(); argidx++) {
+                if (args[argidx] == "-w" && argidx + 2 < args.size()) {
+                    std::string lhs = args[++argidx].c_str();
+                    std::string rhs = args[++argidx].c_str();
+                    weights.emplace_back(lhs, rhs);
+                    continue;
+                }
+                if (args[argidx] == "-i" && argidx + 1 < args.size()) {
+                    std::string arg = args[++argidx].c_str();
+                    max_iter = std::stoul(arg);
+                    continue;
+                }
+                if (args[argidx] == "-v" && argidx + 1 < args.size()) {
+                    std::string arg = args[++argidx].c_str();
+                    test_vectors_n = std::stoul(arg);
+                    continue;
+                }
+                if (args[argidx] == "-d") {
+                    worker.debug = true;
+                    continue;
+                }
+                if (args[argidx] == "-r") {
+                    worker.rewrite_run = true;
+                }
+            }
+            extra_args(args, argidx, design);
+
+            Module *top_mod = nullptr;
+
+            if (design->full_selection()) {
+                top_mod = design->top_module();
+
+                if (!top_mod)
+                    log_cmd_error("Design has no top module, use the 'hierarchy' command to specify one.\n");
+            } else {
+                auto mods = design->selected_whole_modules();
+
+                if (GetSize(mods) != 1)
+                    log_cmd_error("Only one top module must be selected.\n");
+
+                top_mod = mods.front();
+            }
+
+            for (auto &w : weights) {
+                RTLIL::SigSpec lhs;
+                if (!RTLIL::SigSpec::parse_sel(lhs, design, top_mod, w.first))
+                    log_cmd_error("Failed to parse lhs weight expression `%s'.\n", w.first.c_str());
+                if (!lhs.is_wire() || !lhs.as_wire()->port_output)
+                    log_cmd_error("Lhs weight expression `%s' not an output.\n", w.first.c_str());
+
+                worker.weights[lhs] = std::stod(w.second);
+            }
+
+            worker.max_iter = std::stoul(max_iter);
+            worker.test_vectors_n = std::stoul(test_vectors_n);
+
+            worker.run(top_mod);
+
+            log_pop();
+        }
+    } AlsPass;
+
+} // namespace yosys_als
